6efde18051daf4183aa748646a147c011739b871
thiagopbarnabe <thiagopbarnabe@gmail.com>
2016-11-28 14:52:30 -0200

    alteração js

--- banner.js
+++ banner.js
@@ -1,1 +1,2 @@
-alert("teste");
+$("body").append('<a id="linkteste" href="#"><img src="https://raw.githubusercontent.com/thiagopbarnabe/banner/master/banner.png"></a>')
+$("#linkteste").click(function(){ alert("teste")});
